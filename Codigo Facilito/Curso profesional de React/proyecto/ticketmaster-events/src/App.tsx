17379c6f51ce13147db1967319bcedd87a0e5d01
stonedjjh <danjim82@gmail.com>
2026-01-20 08:01:01 -0400

    Fix: Eliminación de código indebido

--- Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/App.tsx
+++ Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/App.tsx
@@ -1,20 +1,11 @@
-import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Events from "./components/Events";
 import "./App.css";
 
-const arrayOfNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 function App() {
-  const [count, setCount] = useState(0);
-  const items: React.ReactElement[] = arrayOfNumbers.map((number) => (
-    <li key={`array-number-item-${number}`}>{`Item ${number}`}</li>
-  ));
-
   return (
     <>
       <Navbar />
-      {/* <ul>{items}</ul> */}
       <Events />
     </>
   );
