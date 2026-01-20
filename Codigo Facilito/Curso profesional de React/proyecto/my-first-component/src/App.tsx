42d255134bd0b792940fd4efec4b6b2e043b692b
stonedjjh <danjim82@gmail.com>
2026-01-20 07:26:24 -0400

    feature: ejemplo de renderizado condicional con operador and

--- Codigo Facilito/Curso profesional de React/proyecto/my-first-component/src/App.tsx
+++ Codigo Facilito/Curso profesional de React/proyecto/my-first-component/src/App.tsx
@@ -15,33 +15,29 @@ function App() {
 
   return (
     <>
-      {/* Ejemplo usando operador ternario */}
-      {value < 2 ? (
-        <div>Cargando...</div>
-      ) : (
-        <div>
-          <div>
-            <a href="https://vite.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-            <p>
-              Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-          </p>
-        </div>
-      )}
+      {/* Ejemplo usando operador logico */}
+      {value < 2 && <div>Cargando...</div>}
+
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
