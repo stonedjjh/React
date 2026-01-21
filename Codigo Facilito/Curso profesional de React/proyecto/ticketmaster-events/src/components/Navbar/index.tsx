31b5b9e5e293ea4edb87ef65b839dcf7af9a2370
stonedjjh <danjim82@gmail.com>
2026-01-21 09:39:02 -0400

    Feature: Implementación de input controlado con useState y tipado de eventos

--- Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/components/Navbar/index.tsx
+++ Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/components/Navbar/index.tsx
@@ -1,15 +1,32 @@
+//Se importa useState para controlar estados en este caso del Input
 import { useState } from "react";
 
 const Navbar = () => {
-  const [search, setSearch] = useState("");
+  /*Se defina la variable que vinculara su estado con el input
+  al ser inmutable React permite modificarla a traves del
+  una funcion que por norma suele llevar el mismo nombre de la
+  variable anteponiendo set
+  Nota aqui estamos usando una destructuración
+  */
+  const [search, setSearch] = useState<string>("");
 
   const handlerInputchange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    /* cuando se dectecta un cambio en el input seteamos su valor a la variable
+       que tenemos vinculada pero esta es inmutable por lo cual se hace a traves
+       de la funcion set
+    */
     setSearch(event.target.value);
+    /*
+    aqui se podria destructurar value pero para ser fiel al curso se dejo como esta
+    const { value } = event.target; 
+    setSearch(value);
+    */
   };
 
   return (
     <>
       <p>Eventos</p>
+      {/* Controlled Component */}
       <input
         placeholder="Busca tu evento favorito"
         onChange={handlerInputchange}
