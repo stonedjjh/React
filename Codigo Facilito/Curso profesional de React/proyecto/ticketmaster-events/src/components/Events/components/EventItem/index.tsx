eb41721de8208b3c1a3b4deee0409e1b8d74682f
stonedjjh <danjim82@gmail.com>
2026-01-26 20:16:21 -0400

    refactor: se acomodaron los tipados de eventos y se agrego una función para manejar el botón  de ver mas

--- Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/components/Events/components/EventItem/index.tsx
+++ Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/components/Events/components/EventItem/index.tsx
@@ -1,24 +1,30 @@
 import React from "react";
-import type { Event } from "../../../../utils/eventInterface";
+import type { Event } from "../../../../utils/EventInterface";
 
 // 1. Definición de la Interfaz de Props Única
 interface EventItemProps {
   event: Event;
   // Usamos el tipo correcto para un manejador de clic en un div
-  // Nota: Lo cambié a HTMLDivElement porque estás usando un <div> en el render
   onEventClick: (id: string) => void;
 }
 
 // 2. Definición del Componente usando React.FC<T> (Recomendado)
 //    y desestructurando todas las props del ÚNICO argumento
 const EventItem: React.FC<EventItemProps> = ({ event, onEventClick }) => {
+  const handleSeeMoreClick = (
+    evt: React.MouseEvent<HTMLButtonElement>,
+  ): void => {
+    evt.stopPropagation();
+    onEventClick(event.id);
+  };
+
   return (
-    // Adjuntamos la función 'onEventClick'
+    // Se adjunta la función 'onEventClick'
     <div>
       <h4>{event.name}</h4>
       <img src={event.image} alt={event.name} width={200} height={200} />
       <p>{event.info}</p>
-      <button onClick={() => onEventClick(event.id)}>Ver más</button>
+      <button onClick={handleSeeMoreClick}>Ver más</button>
     </div>
   );
 };
