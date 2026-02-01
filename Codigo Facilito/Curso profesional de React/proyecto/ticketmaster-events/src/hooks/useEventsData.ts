3354b7f3dfca3550e91a275a28640dfd1010bd1e
stonedjjh <danjim82@gmail.com>
2026-01-31 23:07:49 -0400

    refactor: cambio de useState por useRef

--- Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/hooks/useEventsData.ts
+++ Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/hooks/useEventsData.ts
@@ -1,12 +1,13 @@
-//Se crea un hook personalizado para importar la data que se usara
-import { useState } from "react";
+//Se cambio useState a useRef
+import { useRef } from "react";
 import eventsJSON from "../data/events.json";
 
 const useEventsData = () => {
-  const [data] = useState(eventsJSON);
+  //se quito la destructuración
+  const data = useRef(eventsJSON);
   const {
     _embedded: { events },
-  } = data;
+  } = data.current;
 
   return {
     events,
