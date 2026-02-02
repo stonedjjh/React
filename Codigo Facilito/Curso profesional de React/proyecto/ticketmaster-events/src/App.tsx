44acebdffc3cd6e40fac3e13954e836aca5b230a
stonedjjh <danjim82@gmail.com>
2026-02-02 18:11:14 -0400

    refactor: se movio la logica de app a la vista Home ahora app solo contiene el componenteRroutes

--- Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/App.tsx
+++ Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/App.tsx
@@ -1,23 +1,7 @@
-import { useRef, useState } from "react";
-import Navbar from "./components/Navbar";
-import type { dataImperativeHandle } from "./components/Navbar";
-import Events from "./components/Events";
-import Routes from "./routes";
 import "./App.css";
+import Routes from "./routes";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const containerRef = useRef<dataImperativeHandle>(null);
-
-  const handlerNavbarSearch = (term: string) => {
-    /*
-    aunque no es de uso frecuente aqui se ve que cambiar el estado
-    desde el padre funciona igual que hacerlo desde el hijo
-    */
-    console.log(containerRef.current?.setSearch(""));
-    setSearchTerm(term);
-  };
-
   return <Routes />;
 }
 
--- Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/views/Home/index.tsx
+++ Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/views/Home/index.tsx
@@ -1,3 +1,27 @@
-const Home = () => <h1>Hola a mi home</h1>;
+import { useRef, useState } from "react";
+import Navbar from "../../components/Navbar";
+import type { dataImperativeHandle } from "../../components/Navbar";
+import Events from "../../components/Events";
+
+const Home = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const containerRef = useRef<dataImperativeHandle>(null);
+
+  const handlerNavbarSearch = (term: string) => {
+    /*
+    aunque no es de uso frecuente aqui se ve que cambiar el estado
+    desde el padre funciona igual que hacerlo desde el hijo
+    */
+    console.log(containerRef.current?.setSearch(""));
+    setSearchTerm(term);
+  };
+
+  return (
+    <>
+      <Navbar onSearch={handlerNavbarSearch} ref={containerRef} />
+      <Events searchTerm={searchTerm} />
+    </>
+  );
+};
 
 export default Home;
