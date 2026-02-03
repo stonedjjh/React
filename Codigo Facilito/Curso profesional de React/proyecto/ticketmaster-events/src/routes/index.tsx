23eb5483b51d7e6a71496ba3a716457d42d35de1
stonedjjh <danjim82@gmail.com>
2026-02-02 21:14:46 -0400

    feat: se definieron rutas anidadas dentro de la ruta profile

--- Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/routes/index.tsx
+++ Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "../views/Home";
 import Detail from "../views/Detail";
 import Error404 from "../views/Error404";
+import Profile from "../views/Profile";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,20 @@ const router = createBrowserRouter([
   {
     path: "/detail/:eventId",
     element: <Detail />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+    children: [
+      {
+        path: "/my-info",
+        element: <div>My Info</div>,
+      },
+      {
+        path: "/liked-events",
+        element: <div>Liked Events</div>,
+      },
+    ],
   },
 ]);
 
---
+++ Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/views/Profile/index.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+const Profile = () => {
+  return (
+    <div>
+      Profile
+      <Outlet />
+    </div>
+  );
+};
+
+export default Profile;
