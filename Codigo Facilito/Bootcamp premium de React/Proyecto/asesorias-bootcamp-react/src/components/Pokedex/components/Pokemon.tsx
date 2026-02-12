25f62a47bff879768d7e26d4c66efc7641ac4611
stonedjjh <danjim82@gmail.com>
2026-02-12 06:45:50 -0400

    feat: creación del card para mostrar la información de un pokemon

---
+++ Codigo Facilito/Bootcamp premium de React/Proyecto/asesorias-bootcamp-react/src/components/Pokedex/components/Pokemon.tsx
@@ -0,0 +1,41 @@
+import type { FC } from "react";
+import type { Pokemon } from "../../../types/pokemon";
+
+const PokemonItem: FC<Pokemon> = ({
+  sprites,
+  name,
+  height,
+  weight,
+  base_experience,
+}) => (
+  <div className="bg-white rounded-lg">
+    <div className="p-6 relative">
+      <img
+        src={sprites.other["official-artwork"].front_default}
+        alt={name}
+        className="w-full h-48 object-contain"
+      />
+    </div>
+    <div className="p-4">
+      <div>
+        <h3 className="text-xl font-bold capitalize text-gray-800">{name}</h3>
+      </div>
+      <div className="mt-3 grid grid-cols-3 gap-2 text-center">
+        <div>
+          <p className="text-xs text-gray-500">Altura</p>
+          <p className="font-semibold text-gray-800">{height / 10}m</p>
+        </div>
+        <div>
+          <p className="text-xs text-gray-500">Peso</p>
+          <p className="font-semibold text-gray-800">{weight / 10}kg</p>
+        </div>
+        <div>
+          <p className="text-xs text-gray-500">XP Base</p>
+          <p className="font-semibold text-gray-800">{base_experience}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default PokemonItem;
