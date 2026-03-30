8a3392fe9d1b7ab74891dedc86ec472f4c6162c3
stonedjjh <danjim82@gmail.com>
2026-03-29 23:12:58 -0400

    feat: Se agrego la funcionalidad a los tab del nav y se agregaron sus respectivos estilos.

--- Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/views/Profile/index.tsx
+++ Codigo Facilito/Curso profesional de React/proyecto/ticketmaster-events/src/views/Profile/index.tsx
@@ -1,9 +1,39 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useNavigate, Link } from "react-router";
+
+import styles from "./Profile.module.css";
 
 const Profile = () => {
+  // useLocation es un hook que nos permite acceder a la ubicación actual de la ruta
+  const { pathname } = useLocation();
+  const navigate =  useNavigate();
+
+  //Se agrega el manejador de evento de los tabs para redirigir a la url adecuada
+  const handlerTabClick = (path:string) => {
+         navigate(`/profile/${path}`);
+  };
+
   return (
     <div>
-      Profile
+      {/* Se agrego un Link al home */}
+      <Link to="/" className={styles.homeLink}>Inicio</Link>
+      <div className={styles.tabsContainer}>
+        {/* Con el pathname se verifica la ruta para activar los estilos correspondientes */}
+        <span 
+            className={`${pathname.includes('my-info') ? styles.active : ''} ${styles.tab}`}
+            onClick={ () => handlerTabClick('my-info') }
+            style={
+              {marginRight: 8}
+            }
+            >
+              Mi información
+        </span>
+        <span 
+            className={`${pathname.includes('liked-events') ? styles.active : ''} ${styles.tab}`}
+            onClick={ () => handlerTabClick('liked-events') }
+            >
+              Eventos Favoritos
+        </span>
+      </div>
       <Outlet />
     </div>
   );
